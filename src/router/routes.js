4b801d32d79bc40384aede4dcdfa5a7b016427bd
obitrim <pevangelist516@gmail.com>
2020-03-05 07:07:41 +0000

    added login and register routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,21 @@
 import LandingPage from '@/views/LandingPage.vue';
+import AppLogin from '@/views/auth/AppLogin.vue';
+import AppRegister from '@/views/auth/AppRegister.vue';
 
 export default [
     {
         path: '/',
         name: 'home',
         component: LandingPage
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: AppLogin
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: AppRegister
     }
 ];
