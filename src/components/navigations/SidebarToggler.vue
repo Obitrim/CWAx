40313d0c67a246c6420e7e375ad690a3390f39d8
obitrim <pevangelist516@gmail.com>
2020-03-05 07:07:17 +0000

    added file for sidebar toggler component

---
+++ src/components/navigations/SidebarToggler.vue
@@ -0,0 +1,21 @@
+<template>
+    <a 
+        href="#"
+        class="sidebar_toggler p-2"
+        @click.prevent="onClick"
+        >
+        <i class="fa fa-align-right"></i>
+    </a>
+</template>
+
+<script>
+import { eventBus } from '@/event-bus.js';
+
+export default {
+    methods: {
+        onClick(){
+            eventBus.$emit('sidebar-clicked');
+        }
+    }
+};
+</script>
