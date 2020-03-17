c0643cc89a55173cb3b9ae8c91ee738c649856d3
obitrim <pevangelist516@gmail.com>
2020-03-17 08:53:53 +0000

    added file for dashboard

---
+++ src/views/master/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-4">
+                <div class="p-4 bg-white dash">
+                    Dash 1
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="p-4 bg-white dash">
+                    Dash 2
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="p-4 bg-white dash">
+                    Dash 3
+                </div>
+            </div>
+        </div>
+        <div class="row">Graph</div>
+    </div>
+</template>
