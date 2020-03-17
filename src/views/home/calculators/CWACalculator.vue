bde0357e28e5da546a96e12f3363752a24ab57a5
obitrim <pevangelist516@gmail.com>
2020-03-17 08:48:38 +0000

    deleted file

--- src/views/home/calculators/CWACalculator.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="cwa_calculator">
-        <h1>CWA calculator</h1>
-    </div>
-</template>
--- src/views/home/calculators/ScoreEstimateCalculator.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="score_estimator">
-        <h1>Score Estimator</h1>
-    </div>
-</template>
