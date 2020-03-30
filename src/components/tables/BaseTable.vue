2ac762f790883e6eff389bfbd85ce03799357b63
obitrim <pevangelist516@gmail.com>
2020-03-30 01:53:13 +0000

    added BaseTable component

---
+++ src/components/tables/BaseTable.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="table-responsive">
+        <table class="table" :class="{ 'text-center': centerText }">
+            <slot name="headings"/>
+            <slot/>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+      centerText: {
+          type: Boolean,
+          default: false
+      }
+  }  
+};
+</script>
