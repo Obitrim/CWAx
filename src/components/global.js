857e5487c23329c601260c4622b61228cdd85238
obitrim <pevangelist516@gmail.com>
2020-03-30 01:52:39 +0000

    registered Global BaseTable component

--- src/components/global.js
+++ src/components/global.js
@@ -1,7 +1,9 @@
+import BaseTable from '@/components/tables/BaseTable.vue';
 import BaseButton from '@/components/buttons/BaseButton.vue';
 import BasePasswordField from '@/components/form/BasePasswordField.vue';
 
 export default {
+    'base-table': BaseTable,
     'base-button': BaseButton,
-    'base-password-field': BasePasswordField
+    'base-password-field': BasePasswordField,
 };
