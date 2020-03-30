d46ecb37c7f425015e117df5ae062d52153ab3fc
obitrim <pevangelist516@gmail.com>
2020-03-30 01:54:19 +0000

    improved component template

--- src/views/master/Index.vue
+++ src/views/master/Index.vue
@@ -1,22 +1,31 @@
 <template>
-    <div class="container my-3">
+    <div class="container my-3 dashboard">
         <div class="row">
-            <div class="col-4">
-                <div class="p-4 bg-white dash">
-                    Dash 1
+            <div class="col-md-6 col-lg-4 my-2">
+                <div class="p-4 text-center bg-white dash">
+                    <h6>CWA</h6>
+                    <h2>68.17</h2>
                 </div>
             </div>
-            <div class="col-4">
-                <div class="p-4 bg-white dash">
-                    Dash 2
+            <div class="col-md-6 col-lg-4 my-2">
+                <div class="p-4 text-center bg-white dash">
+                    <h6>Accumulated Credit Hours</h6>
+                    <h2>91</h2>
                 </div>
             </div>
-            <div class="col-4">
-                <div class="p-4 bg-white dash">
-                    Dash 3
+            <div class="col-md-6 col-lg-4 my-2">
+                <div class="p-4 text-center bg-white dash">
+                    <h6>Accumulated Scores</h6>
+                    <h2>6643</h2>
                 </div>
             </div>
         </div>
-        <div class="row">Graph</div>
+        <div class="row">
+            <div class="col my-5">
+                <div class="p-4 bg-white dash graph_dash">
+                    <h6>Study pattern</h6>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
